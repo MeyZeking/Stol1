cd746aee3c5940dff3a9a77749b67c25535bbbb5
Isaev Kirill <lucifer.denicze96@mail.ru>
2023-04-04 18:14:50 +0300

    animation

--- script/script2.js
+++ script/script2.js
@@ -25,6 +25,45 @@ document.addEventListener('DOMContentLoaded', function() {
 
       console.log(sliderItems);
   });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    const anchors = document.querySelectorAll('a[href*="#"]');
+    for (let anchor of anchors) {
+      anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        const blockID = anchor.getAttribute('href').substr(1);
+        const headerHeight = document.querySelector('header').offsetHeight;
+        const scrollToBlock = document.getElementById(blockID).getBoundingClientRect().top + window.pageYOffset - headerHeight;
+        window.scrollTo({
+          top: scrollToBlock,
+          behavior: 'smooth'
+        });
+      });
+    }
+  });
+
   
 
-
+document.addEventListener('DOMContentLoaded', function() {
+    const modal = document.getElementById('modal');
+    const closeButton = document.querySelector('.close');
+    const loginButton = document.querySelector('.login-button');
+    console.log(loginButton);
+  
+    // Открываем модальное окно при клике на кнопку "Войти"
+    loginButton.addEventListener('click', function() {
+      modal.style.display = 'block';
+    });
+  
+    // Закрываем модальное окно при клике на кнопку "Закрыть"
+    closeButton.addEventListener('click', function() {
+      modal.style.display = 'none';
+    });
+  
+    // Закрываем модальное окно при клике за его пределами
+    window.addEventListener('click', function(event) {
+      if (event.target == modal) {
+        modal.style.display = 'none';
+      }
+    });
+  });
