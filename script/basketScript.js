81584abc008d50bdc9f3af1783e293d200f426f2
Isaev Kirill <lucifer.denicze96@mail.ru>
2023-04-17 16:12:30 +0300

    basket2

--- script/basketScript.js
+++ script/basketScript.js
@@ -1,5 +1,8 @@
 const addButton = document.getElementById('add');
 const minusButton = document.getElementById('minus');
+const dish = document.querySelector('.dish-icon_block');
+const buttonShowBread = document.getElementById('add-bread');
+const breadBlock = document.getElementById('bread');
 
 const countDish = document.querySelector('#count');
 let number = parseInt(countDish.textContent);
@@ -10,6 +13,9 @@ function incrementNumber(){
 }
 
 function discrementNumber(){
+    if(number < 2){
+        dish.style.display = 'none';
+    }
     number--;
     console.log(number);
     countDish.textContent = number;
@@ -17,3 +23,13 @@ function discrementNumber(){
 
 addButton.addEventListener('click', incrementNumber);
 minusButton.addEventListener('click', discrementNumber);
+
+buttonShowBread.addEventListener('click', function(){
+    breadBlock.classList.toggle('visible');
+    if(breadBlock.classList.contains('visible')){
+        buttonShowBread.classList.toggle('active');
+    }
+    else{
+        buttonShowBread.classList.remove('active');
+    }
+});
